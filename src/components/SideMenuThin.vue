645160dc45dd0c2df7beaaaba0d0f636f2ce695e
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-05 00:21:42 +0100

    Created sidemenu(thin) component.

---
+++ src/components/SideMenuThin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="border-r w-14 bg-gray-800 text-center py-5"
+    @click="hideAll($event)"
+  >
+    <a href="#" class="block text-white mb-20 hover:text-yellow-500">
+      <font-awesome-icon icon="bars" />
+    </a>
+    <a href="#" class="block text-white mb-5 hover:text-yellow-500">
+      <font-awesome-icon icon="list" />
+    </a>
+    <a href="#" class="block text-white mb-5 hover:text-yellow-500">
+      <font-awesome-icon icon="users" />
+    </a>
+    <a href="#" class="block text-white mb-5 hover:text-yellow-500">
+      <font-awesome-icon icon="chart-pie" />
+    </a>
+    <a href="#" class="block text-white mb-5 hover:text-yellow-500">
+      <font-awesome-icon icon="calendar-check" />
+    </a>
+    <a href="#" class="block bg-red-600 text-white py-4 mb-5 hover:text-white">
+      <font-awesome-icon icon="sign-out-alt" />
+    </a>
+  </div>
+</template>
