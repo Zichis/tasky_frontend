b1cc77a6160be1910fd4915ee2c79d7ad23a822b
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-04 12:16:08 +0100

    Imported font-awesome.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,38 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./css/index.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBars,
+  faList,
+  faUsers,
+  faChartPie,
+  faCalendarCheck,
+  faSignOutAlt,
+  faSearch,
+  faEnvelope,
+  faBell,
+  faUser,
+  faCommentAlt,
+  faPaperclip,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faBars,
+  faList,
+  faUsers,
+  faChartPie,
+  faCalendarCheck,
+  faSignOutAlt,
+  faSearch,
+  faEnvelope,
+  faBell,
+  faUser,
+  faCommentAlt,
+  faPaperclip
+);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
