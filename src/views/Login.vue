5052e240d93d67818f4271d0c3647e9fa0c0b3c5
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-11 17:02:35 +0100

    Added style to login page.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,37 +1,64 @@
 <template>
-  <div class="flex justify-center items-center h-screen">
-    <form @submit.prevent="login">
-      <div v-if="loginErrorMessage !== null">
-        <p class="my-1 text-red-400 font-light">{{ loginErrorMessage }}</p>
-      </div>
-      <div class="mb-5">
-        <label for="email" class="block mb-2">Email</label>
-        <input
-          class="px-2 py-1 border-2 border-gray-300 rounded focus:outline-none focus:border-gray-400"
-          type="text"
-          name="email"
-          v-model="loginForm.email"
-          placeholder="Enter email"
-          autofocus
-        />
-      </div>
-      <div class="mb-5">
-        <label for="password" class="block mb-2">Password</label>
-        <input
-          class="px-2 py-1 border-2 border-gray-300 rounded focus:outline-none focus:border-gray-400"
-          type="password"
-          name="password"
-          v-model="loginForm.password"
-          placeholder="Enter password"
-        />
-      </div>
-      <button
-        class="bg-blue-400 font-light px-3 py-1 rounded text-blue-100 hover:text-white hover:bg-blue-500"
-        type="submit"
-      >
-        Login
-      </button>
-    </form>
+  <div
+    class="flex-1 bg-gray-50 flex justify-center items-center px-5 md:px-10 h-screen"
+  >
+    <div class="w-full">
+      <header class="px-5 text-gray-700 mb-8">
+        <h2 class="text-2xl font-semibold mb-10">
+          Login
+        </h2>
+        <h3 class="font-semibold mb-2">Log into your account</h3>
+        <p class="font-light">
+          Welcome back to Tasky. Enter your account details to continue from
+          where you stopped.
+        </p>
+      </header>
+      <form @submit.prevent="login" class="w-full">
+        <div v-if="loginErrorMessage !== null">
+          <p class="my-1 px-5 text-red-400 font-light">
+            {{ loginErrorMessage }}
+          </p>
+        </div>
+        <div class="flex flex-wrap">
+          <div class="w-full lg:w-1/2 px-5 mb-5">
+            <label for="email" class="block mb-2 text-gray-500">Email</label>
+            <input
+              class="px-2 py-1 w-full border-2 border-gray-300 rounded focus:outline-none focus:border-gray-400"
+              type="text"
+              name="email"
+              v-model="loginForm.email"
+              placeholder="Enter email"
+            />
+          </div>
+          <div class="w-full lg:w-1/2 px-5 mb-5">
+            <label for="password" class="block mb-2 text-gray-500"
+              >Password</label
+            >
+            <input
+              class="px-2 py-1 w-full border-2 border-gray-300 rounded focus:outline-none focus:border-gray-400"
+              type="password"
+              name="password"
+              v-model="loginForm.password"
+              placeholder="Enter password"
+            />
+          </div>
+          <button
+            class="bg-blue-400 font-light px-3 py-1 mb-2 mx-5 rounded text-blue-100 hover:text-white hover:bg-blue-500"
+            type="submit"
+          >
+            Login
+          </button>
+        </div>
+        <p class="px-5 font-light text-gray-500">
+          You don't have an account?
+          <router-link
+            :to="{ name: 'Register' }"
+            class="font-normal text-blue-400 hover:text-blue-500"
+            >Sign Up</router-link
+          >
+        </p>
+      </form>
+    </div>
   </div>
 </template>
 
