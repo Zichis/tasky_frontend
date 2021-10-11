332e62f11995f3a48f533fb53b050a2bc921b7ca
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-11 21:24:14 +0100

    Created input component.

---
+++ src/components/Form/Input.vue
@@ -0,0 +1,9 @@
+<template>
+  <input
+    class="px-2 py-1 w-full border-2 border-gray-300 rounded focus:outline-none focus:border-gray-400"
+  />
+</template>
+
+<script>
+export default {};
+</script>
