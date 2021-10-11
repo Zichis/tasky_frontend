6fb9254ccb7d7f48185b04f529a9785894a48771
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-11 20:23:40 +0100

    Added links to sidemenu.

--- src/components/SideMenuLarge.vue
+++ src/components/SideMenuLarge.vue
@@ -1,20 +1,27 @@
 <template>
-  <div
-    class="border-r w-64 bg-gray-200 absolute h-screen left-14 z-10"
-    @click="hideAll($event)"
-  >
+  <div class="border-r w-64 bg-gray-200 absolute h-screen left-14 z-10">
     <h2 class="text-center py-5 text-2xl font-semibold text-gray-700">
       My Tasks
     </h2>
+    <div class="text-center py-5">
+      <router-link
+        :to="{ name: 'CreateTask' }"
+        class="bg-gray-500 shadow-md px-3 py-2 rounded-full font-light text-gray-200 hover:bg-gray-600 hover:text-white transition-colors"
+        >Add Task</router-link
+      >
+    </div>
     <div>
-      <a href="#" class="flex justify-between items-center px-5 mb-7">
+      <router-link
+        :to="{ name: 'Tasks' }"
+        class="flex justify-between items-center px-5 mb-7"
+      >
         <h4>All Tasks</h4>
         <div
           class="bg-blue-500 text-white text-xs font-light py-1 px-2 rounded-full"
         >
           99+
         </div>
-      </a>
+      </router-link>
       <a
         href="#"
         class="flex justify-between items-center px-5 mb-7 text-gray-700"
