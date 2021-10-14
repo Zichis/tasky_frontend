d196187aba61895d97b035546bff273097dd5665
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-14 12:31:37 +0100

    Added delete feature.

--- src/views/Tasks/Index.vue
+++ src/views/Tasks/Index.vue
@@ -1,20 +1,39 @@
 <template>
-  <div class="flex flex-wrap">
-    <task-card v-for="task in tasks" :key="task.id" :task="task"></task-card>
+  <div>
+    <div
+      v-if="noTasks"
+      class="flex justify-center items-center font-light h-60"
+    >
+      You have no tasks.&nbsp;
+      <router-link
+        :to="{ name: 'CreateTask' }"
+        class="font-normal text-blue-500 hover:text-blue-600"
+        >Add a task.</router-link
+      >
+    </div>
+    <div class="flex flex-wrap">
+      <task-card
+        v-for="task in tasks"
+        :key="task.id"
+        :task="task"
+        @deleteConfirm="deleteConfirm"
+      ></task-card>
+    </div>
   </div>
 </template>
 
 <script>
 import TaskCard from "@/components/TaskCard";
 import axios from "axios";
-import GetAlert from "../../functions/GetAlert";
-import DeleteAlert from "../../functions/DeleteAlert";
+import SetAlert from "../../functions/SetAlert";
+import DisplayAlert from "../../functions/DisplayAlert";
 
 export default {
   components: { TaskCard },
   data() {
     return {
       tasks: [],
+      noTasks: false,
       headers: {
         Accept: "application/json",
         Authorization: "Bearer " + localStorage.getItem("myapp_token"),
@@ -26,15 +45,40 @@ export default {
       .get("http://myapi.test/api/tasks", { headers: this.headers })
       .then((response) => {
         this.tasks = response.data;
+        if (this.tasks.length < 1) {
+          this.noTasks = true;
+        }
       })
       .catch((error) => console.log(error.response.data.message));
 
-    let [alertAction, alertMessage, alertStatus] = GetAlert();
-
-    if (alertMessage !== null) {
-      this.$swal(alertAction, alertMessage, alertStatus);
-      DeleteAlert();
-    }
+    DisplayAlert(this.$swal);
+  },
+  methods: {
+    deleteConfirm(id) {
+      this.$swal({
+        title: "Warning",
+        icon: "warning",
+        html: "<p>Are you sure you want to delete this task?</>",
+        showDenyButton: true,
+        confirmButtonText: "Yes",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axios
+            .delete(`http://myapi.test/api/tasks/${id}`, {
+              headers: this.headers,
+            })
+            .then((response) => {
+              this.tasks = response.data;
+              SetAlert("Deleted", "Your task has been deleted!", "success");
+              DisplayAlert(this.$swal);
+              if (this.tasks.length < 1) {
+                this.noTasks = true;
+              }
+            })
+            .catch((error) => console.log(error));
+        }
+      });
+    },
   },
 };
 </script>
