456322209a0c33e7bc247ccad431f21ee375ee2b
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-20 11:43:25 +0100

    Updated store mutations, actions, getters.

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,48 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const state = {
+  user: null,
+  tasks: [],
+  categories: [],
+};
 
 export default new Vuex.Store({
-  state: {
-  },
+  state,
   mutations: {
+    user(state, user) {
+      state.user = user;
+    },
+    tasks(state, tasks) {
+      state.tasks = tasks;
+    },
+    categories(state, categories) {
+      state.categories = categories;
+    },
   },
   actions: {
+    user(context, user) {
+      context.commit("user", user);
+    },
+    tasks(context, tasks) {
+      context.commit("tasks", tasks);
+    },
+    categories(context, categories) {
+      context.commit("categories", categories);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+  getters: {
+    user: (state) => {
+      return state.user;
+    },
+    tasks: (state) => {
+      return state.tasks;
+    },
+    categories: (state) => {
+      return state.categories;
+    },
+  },
+});
