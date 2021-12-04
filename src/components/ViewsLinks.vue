781643152864990d04c0c33a6b64644d9532463b
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-12-04 13:05:18 +0100

    Created components for view links.

---
+++ src/components/ViewsLinks.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="px-5 text-right text-gray-500 mb-5">
+    <router-link class="mr-5 text-xl hover:text-gray-800" to="/"
+      ><font-awesome-icon icon="th-large"
+    /></router-link>
+    <router-link class="text-xl hover:text-gray-800" to="/tasks-list"
+      ><font-awesome-icon icon="th-list"
+    /></router-link>
+  </div>
+</template>
